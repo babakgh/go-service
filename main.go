e7e970fccf252b0de32fe5838452e0f5d2314f2a
Khash Sajadi <khash@cloud66.com>
2015-07-24 21:02:15 +0100

    basic web server

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "os"
+	"net/http"
+  "fmt"
+
+	"github.com/ant0ine/go-json-rest/rest"
+)
+
+func main() {
+  handler := rest.ResourceHandler{}
+
+	err := handler.SetRoutes(
+		// system
+		&rest.Route{"GET", "/ping", func(w rest.ResponseWriter, r *rest.Request) {
+      w.WriteJson("ok")
+    }},
+	)
+	if err != nil {
+    fmt.Printf(err.Error())
+		os.Exit(2)
+	}
+
+  url := "0.0.0.0:8080"
+	fmt.Printf("Starting API on %s", url)
+	if err := http.ListenAndServe(url, &handler); err != nil {
+		fmt.Printf("Failed to start API %s", err.Error())
+		os.Exit(2)
+	}
+}
